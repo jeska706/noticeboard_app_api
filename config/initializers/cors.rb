3489ab1f5629b98a4837a153c94add93dd413784
Jessica Beardsley <jessicabeardsley@hotmail.com>
2017-01-24 15:42:20 -0700

    Added example.com

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:3001' #'*' for any request
+    origins 'example.com' #'*' for any request
 
     resource '*',
       headers: :any,
